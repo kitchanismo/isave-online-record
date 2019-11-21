46ae96a43ecc43fd1b49c342b72ea4e735ae1b77
kitchan <cristian.betsayda@gmail.com>
2019-11-22 06:03:21 +0800

    test

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -11,7 +11,7 @@ axios.interceptors.response.use(
 const getAPIUrl = () => {
 	return process.env.NODE_ENV === 'development'
 		? 'http://127.0.0.1:3333'
-		: process.env.PUBLIC_URL
+		: 'https://infomatech.herokuapp.com'
 }
 
 axios.interceptors.request.use(config => {
